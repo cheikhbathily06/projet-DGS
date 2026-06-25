148b81f7e15aaa16fafc8626a523045efd489cdb
Amala <cheikhbath59@gmail.com>
2026-06-25 15:26:13 +0000

    fix nginx php-fpm communication

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
-# Lance les migrations
+php /var/www/artisan config:clear
+php /var/www/artisan cache:clear
 php /var/www/artisan migrate --force
+php /var/www/artisan storage:link
 
 # Démarre php-fpm en arrière-plan
 php-fpm -D
+
+# Attendre que php-fpm soit prêt
+sleep 2
 
 # Démarre nginx au premier plan
 nginx -g "daemon off;"
