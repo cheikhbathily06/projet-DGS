373029450e8eb25039beb75bf13dffb7f3d77dbb
Amala <cheikhbath59@gmail.com>
2026-06-25 15:35:18 +0000

    test deploiement

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -6,13 +6,17 @@ php /var/www/artisan cache:clear
 php /var/www/artisan migrate --force
 php /var/www/artisan storage:link
 
+# Utilise le port Railway ou 80 par défaut
+PORT=${PORT:-80}
+
+# Remplace le port dans la config nginx
+sed -i "s/listen 80/listen $PORT/" /etc/nginx/sites-available/default
+
 # Démarre php-fpm en arrière-plan
 php-fpm -D
 
-# Attendre que php-fpm soit prêt
 sleep 2
 
-# Test config nginx
 nginx -t
 
 # Démarre nginx au premier plan
